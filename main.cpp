3779d42d023eb8d38d4c34b643dfaf7c9dc7465d
Jesus <321307792+DoctorJeffyReal@users.noreply.github.com>
2026-08-27 22:13:35 +0000

    change some part of my story

--- main.cpp
+++ main.cpp
@@ -15,6 +15,6 @@ int main() {
             << "and gain a solid understanding of programming concepts.\n"; 
   std::cout << "In the 3rd year, I want to start working on advanced programming concepts.\n";
  std::cout << "In the 5th year, I want to work on building my own projects and contributing to open-source projects.\n"; 
- std::cout << "In the 7th year, I want to work on developing my own software applications and start applying what I've learned in real-world scenarios.\n";
+ std::cout << "In the 7th year, I want to work on developing my own software applications and start applying what I've learned in a work environment.\n";
   return 0;
 }
